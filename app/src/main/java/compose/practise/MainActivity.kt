9d8f8b02ed3ad62a048d87a727dd14df03878e64
4bnetworkAkshay <114798801+4bnetworkAkshay@users.noreply.github.com>
2023-07-08 16:28:42 +0530

    Image composable added

--- app/src/main/java/compose/practise/MainActivity.kt
+++ app/src/main/java/compose/practise/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import compose.practise.ui.theme.ComposePractiseTheme
 import kotlin.random.Random
@@ -23,7 +28,7 @@ class MainActivity : ComponentActivity() {
             if (random < 5)
                 printTitle(title = "Akshay kumar")
             else
-                printMessage(title = "Message")
+                showImage()
         }
     }
 
@@ -33,7 +38,15 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun printMessage(title: String) {
-        Text(text = "Hey this is my $title.")
+    fun showImage() {
+        Row {
+            Text(text = "Hey this is my Image.")
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "DummyImage",
+                Modifier.background(color = Color.Blue)
+            )
+        }
+
     }
 }
