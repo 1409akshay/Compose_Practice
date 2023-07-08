c2e6b113864dd1bddd40712dbbbef62dee3d2bb6
4bnetworkAkshay <114798801+4bnetworkAkshay@users.noreply.github.com>
2023-07-08 18:14:14 +0530

    Added ListView with Scrollable column & RecyclerView with Lazy Column

--- app/src/main/java/compose/practise/MainActivity.kt
+++ app/src/main/java/compose/practise/MainActivity.kt
@@ -9,12 +9,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -22,7 +25,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
@@ -39,11 +41,45 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
+            val list = arrayListOf<ListItemData>()
+            list.add(ListItemData(R.drawable.icon, "Akshay kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Manoj kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Utkarsh kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Manohar kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Pradeep kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Vaibhav kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Abhinav kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Javed kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Ayush kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Kanish kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Tanuj kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Ankit kumar", "Senior Software Developer"))
+            list.add(ListItemData(R.drawable.icon, "Umrao kumar", "Senior Software Developer"))
+
+            //Recycler view of compose
+            LazyColumn(content = {
+                item {
+                    list.map { i ->
+                        showCardItem(img = i.img, title = i.title, subTitle = i.subTitle)
+                    }
+                }
+            })
+
+
+            //Non recyclable list which will create each item whether displayed on screen or not
+            /*Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                    list.map { item ->
+                        showCardItem(img = item.img, title = item.title, subTitle = item.subTitle)
+                    }
+                }*/
+
+            /*Column {
                 showComposableViews()
                 showCardItem()
                 modifierExample()
-            }
+            }*/
+
+
         }
     }
 
@@ -53,9 +89,9 @@ class MainActivity : ComponentActivity() {
             painter = painterResource(id = android.R.drawable.ic_delete),
             contentDescription = "",
             Modifier
-                .clickable {  }
+                .clickable { }
                 .size(200.dp)
-                .border(2.dp, Color.Gray , CircleShape)
+                .border(2.dp, Color.Gray, CircleShape)
                 .shadow(2.dp, RectangleShape, true, Color.Blue)
                 .clip(CircleShape)
 
@@ -63,10 +99,12 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun showCardItem() {
-        Row {
+    private fun showCardItem(img: Int, title: String, subTitle: String) {
+        Row(Modifier.clickable {
+            Toast.makeText(this@MainActivity, title, Toast.LENGTH_SHORT).show()
+        }) {
             Image(
-                painter = painterResource(id = R.drawable.icon),
+                painter = painterResource(id = img),
                 contentDescription = "icon",
                 Modifier
                     .size(100.dp)
@@ -75,12 +113,12 @@ class MainActivity : ComponentActivity() {
                 )
             Column {
                 Text(
-                    text = "Akshay kumar",
+                    text = title,
                     fontSize = 18.sp,
                     modifier = Modifier.padding(5.dp, 15.dp, 10.dp, 0.dp)
                 )
                 Text(
-                    text = "Android Application Developer",
+                    text = subTitle,
                     fontSize = 14.sp,
                     modifier = Modifier.padding(5.dp, 5.dp, 10.dp, 0.dp)
                 )
@@ -117,4 +155,8 @@ class MainActivity : ComponentActivity() {
         val random = Random.nextInt(0, 20)
         Toast.makeText(this@MainActivity, "Clicked $random", Toast.LENGTH_SHORT).show()
     }
+
+    data class ListItemData(val img: Int, val title: String, val subTitle: String)
+
+
 }
