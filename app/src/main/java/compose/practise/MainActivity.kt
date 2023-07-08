d41f80babbfabd8a641a15e0dd130cf3bc09c297
4bnetworkAkshay <114798801+4bnetworkAkshay@users.noreply.github.com>
2023-07-08 16:36:05 +0530

    Button & ImageButton composable added

--- app/src/main/java/compose/practise/MainActivity.kt
+++ app/src/main/java/compose/practise/MainActivity.kt
@@ -6,14 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import compose.practise.ui.theme.ComposePractiseTheme
@@ -23,30 +26,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val random = Random.nextInt(0, 10)
-            Toast.makeText(this, random.toString(), Toast.LENGTH_LONG).show()
-            if (random < 5)
-                printTitle(title = "Akshay kumar")
-            else
-                showImage()
+            showComposableViews()
         }
     }
 
     @Composable
-    fun printTitle(title: String) {
-        Text(text = "Hey this is $title.")
-    }
-
-    @Composable
-    fun showImage() {
-        Row {
+    fun showComposableViews() {
+        Column {
             Text(text = "Hey this is my Image.")
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_foreground),
                 contentDescription = "DummyImage",
                 Modifier.background(color = Color.Blue)
             )
+            Button(onClick = {
+                clickEvent()
+            }) {
+                Text(text = "Click Me")
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "Button",
+                )
+            }
         }
 
+    }
+
+    private fun clickEvent() {
+        val random = Random.nextInt(0, 20)
+        Toast.makeText(this@MainActivity, "Clicked $random", Toast.LENGTH_SHORT).show()
     }
 }
